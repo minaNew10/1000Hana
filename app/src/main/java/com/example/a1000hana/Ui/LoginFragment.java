dc002c5f7ba7cfe413c96a62c7c1b710bee7e2db
MinaN <minadevdream@gmail.com>
2020-02-24 17:27:55 +0200

    move login into fragment

--- app/src/main/java/com/example/a1000hana/Ui/LoginFragment.java
+++ app/src/main/java/com/example/a1000hana/Ui/LoginFragment.java
@@ -1,16 +1,32 @@
 package com.example.a1000hana.Ui;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a1000hana.R;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
@@ -22,6 +38,13 @@ import com.example.a1000hana.R;
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
+    private static final String TAG = "LoginFragment";
+    FirebaseAuth mFirebaseAuth;
+    EditText mEtxtEmail;
+    EditText mEtxtvPsswrd;
+    Button mBtnLogin;
+    TextView mTxtvRegister;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -56,6 +79,22 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser user=mFirebaseAuth.getCurrentUser();
+        if (user==null)
+            Log.d(TAG, "register now: ");
+        else
+            Log.d(TAG, "session exist: ");
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
@@ -68,7 +107,29 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        TextView txtvTitle = v.findViewById(R.id.txtv_app_name_login);
+        TextView btnLogin = v.findViewById(R.id.btn_login);
+        Typeface face = Typeface.createFromAsset(getActivity().getAssets(),"fonts/Adore You.ttf");
+        txtvTitle.setTypeface(face);
+        btnLogin.setTypeface(face);
+        mEtxtEmail = v.findViewById(R.id.etxt_email_login);
+        mEtxtvPsswrd = v.findViewById(R.id.etxt_psswrd_login);
+        mBtnLogin = v.findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+        mTxtvRegister = v.findViewById(R.id.txtv_register);
+        mTxtvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -108,5 +169,54 @@ public class LoginFragment extends Fragment {
     public interface OnLoginFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    void register(){
+        mFirebaseAuth.createUserWithEmailAndPassword(mEtxtEmail.getText().toString(),mEtxtvPsswrd.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getActivity(),"register successful",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Log.d(TAG, "onCanceled: ");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(e instanceof FirebaseAuthWeakPasswordException){
+
+                    Log.d(TAG, "onFailure: "+e.getMessage());
+                }
+            }
+        });
+    }
+
+    void login(){
+        mFirebaseAuth.signInWithEmailAndPassword(mEtxtEmail.getText().toString(),mEtxtvPsswrd.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getActivity(),"login successful",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Log.d(TAG, "onCanceled: ");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: "+e.getMessage());
+            }
+        });
     }
 }
